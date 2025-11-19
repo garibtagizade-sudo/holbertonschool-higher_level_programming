90b254d0248fddf16f41f5a0a95137ab8c800641
garibtagizade-sudo <garibtagizade@gmail.com>
2025-11-19 16:12:34 +0000

    salam

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            new_string += ""
+        else:
+            new_string += i
+    return new_string
