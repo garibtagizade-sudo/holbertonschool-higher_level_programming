5ff3f73767c268d2a34f161ffc248c36f358a5c8
garibtagizade-sudo <garibtagizade@gmail.com>
2025-11-20 13:48:15 +0000

    salam

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
     new = f(tuple_a)
     new1 = f(tuple_b)
     return (new[0] + new1[0], new[1] + new1[1])
-    
+
+
 def f(t=()):
-    if len(t) == 2:
-        return(t[0],t[1])
-    elif len(t) < 2:
-     return( t[0],0)
-    elif len(t) > 2:
-        return (t[0],t[1])
+    # tuple uzunluğu 0-dırsa
+    if len(t) == 0:
+        return (0, 0)
+    # tuple uzunluğu 1-dirsə
+    elif len(t) == 1:
+        return (t[0], 0)
+    # tuple uzunluğu 2 və ya daha çoxdursa
+    else:
+        return (t[0], t[1])
