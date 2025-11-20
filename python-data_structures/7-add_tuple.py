503ca8f036c20857180cf0583d017cbf8f379c89
garibtagizade-sudo <garibtagizade@gmail.com>
2025-11-20 13:40:50 +0000

    salam

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    new = f(tuple_a)
+    new1 = f(tuple_b)
+    return (new[0] + new1[0], new[1] + new1[1])
+    
+def f(t=()):
+    if len(t) == 2:
+        return(t[0],t[1])
+    elif len(t) < 2:
+     return( t[0],0)
+    elif len(t) > 2:
+        return (t[0],t[1])
